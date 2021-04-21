c312ca7eb8e1a53d2b4345fdd07539cf1c008992
Yothin Muangsommuk <m@yothinix.com>
2021-04-21 21:40:02 +0700

    Add configuration for Jaxb2Marshaller to convert XML object to POJO and SOAPConnector client

---
+++ src/main/java/com/example/soap_demo/Config.java
@@ -0,0 +1,24 @@
+package com.example.soap_demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class Config {
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("com.example.soap_demo.model");
+        return marshaller;
+    }
+
+    @Bean
+    public SOAPConnector soapConnector(Jaxb2Marshaller marshaller) {
+        SOAPConnector client = new SOAPConnector();
+        client.setDefaultUri("https://www.dataaccess.com/webservicesserver/NumberConversion.wso");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+}
