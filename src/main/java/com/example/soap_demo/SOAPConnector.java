f1bb25b48327ae2f1bdeae66fcb3fa40b6c9d48a
Yothin Muangsommuk <m@yothinix.com>
2021-04-21 21:40:28 +0700

    Implementing generic SOAP client interface

---
+++ src/main/java/com/example/soap_demo/SOAPConnector.java
@@ -0,0 +1,9 @@
+package com.example.soap_demo;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+public class SOAPConnector extends WebServiceGatewaySupport {
+    public Object callWebService(String url, Object request){
+        return getWebServiceTemplate().marshalSendAndReceive(url, request);
+    }
+}
