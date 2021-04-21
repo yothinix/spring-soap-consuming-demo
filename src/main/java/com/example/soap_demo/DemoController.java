c262ec588d0b7bf46b0429476cc860cbf905f8ec
Yothin Muangsommuk <m@yothinix.com>
2021-04-21 21:44:42 +0700

    Modify controller for more complex

--- src/main/java/com/example/soap_demo/DemoController.java
+++ src/main/java/com/example/soap_demo/DemoController.java
@@ -4,6 +4,7 @@ import com.example.soap_demo.model.NumberToWords;
 import com.example.soap_demo.model.NumberToWordsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +13,10 @@ public class DemoController {
     @Autowired
     SOAPConnector soapConnector;
 
-    @GetMapping("/")
-    public String getStuff() {
+    @GetMapping("/{number}")
+    public String getStuff(@PathVariable String number) {
         NumberToWords numberToWords = new NumberToWords();
-        numberToWords.setUbiNum("500");
+        numberToWords.setUbiNum(number);
 
         String url = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso";
 
