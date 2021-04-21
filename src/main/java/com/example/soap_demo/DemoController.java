0f2b375b30710066e3e881eb9224b7b0216f598d
Yothin Muangsommuk <m@yothinix.com>
2021-04-21 21:40:58 +0700

    Example using SOAPConnection client in controller

--- src/main/java/com/example/soap_demo/DemoController.java
+++ src/main/java/com/example/soap_demo/DemoController.java
@@ -1,13 +1,26 @@
 package com.example.soap_demo;
 
+import com.example.soap_demo.model.NumberToWords;
+import com.example.soap_demo.model.NumberToWordsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Autowired
+    SOAPConnector soapConnector;
+
     @GetMapping("/")
     public String getStuff() {
-        return "Banana";
+        NumberToWords numberToWords = new NumberToWords();
+        numberToWords.setUbiNum("500");
+
+        String url = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso";
+
+        NumberToWordsResponse response = (NumberToWordsResponse) soapConnector.callWebService(url, numberToWords);
+
+        return response.getNumberToWordsResult();
     }
 }
